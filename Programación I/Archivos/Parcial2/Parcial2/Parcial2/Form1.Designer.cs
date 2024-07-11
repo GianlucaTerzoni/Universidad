f8751634e2b9219edcd30dd1dce5b90b013e14f2
GianlucaTerzoni <gianlucaterzoni@hotmail.com>
2024-07-11 12:05:25 -0300

    Parcial - 2 Archivos, corte de control y apareo de archivos

---
+++ Programación I/Archivos/Parcial2/Parcial2/Parcial2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Parcial2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lsbComprobantes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGenerar.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnGenerar.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnGenerar.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnGenerar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerar.Location = new System.Drawing.Point(96, 44);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(121, 61);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.TabStop = false;
+            this.btnGenerar.Text = "GENERAR COMPROBANTES";
+            this.btnGenerar.UseVisualStyleBackColor = false;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSalir.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSalir.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalir.Location = new System.Drawing.Point(96, 141);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(121, 29);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.TabStop = false;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lsbComprobantes
+            // 
+            this.lsbComprobantes.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsbComprobantes.FormattingEnabled = true;
+            this.lsbComprobantes.ItemHeight = 16;
+            this.lsbComprobantes.Location = new System.Drawing.Point(268, 44);
+            this.lsbComprobantes.Name = "lsbComprobantes";
+            this.lsbComprobantes.Size = new System.Drawing.Size(492, 324);
+            this.lsbComprobantes.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lsbComprobantes);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ListBox lsbComprobantes;
+    }
+}
+
---
+++ Programación I/Archivos/Parcial2/Parcial2/Parcial2/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            lsbComprobantes.Items.Clear();
+
+            string filePathCabecera = "CABECERA.txt.txt";
+            string filePathDetalle = "DETALLE.txt.txt";
+
+            
+            string[] vectorCabecera = new string[0];
+            string[] vectorDetalle = new string[0];
+            string nroComprobanteCabecera, razonSocial, fecha, nroComprobanteDetalle, descripcion;
+
+
+
+            try
+            {
+                using (StreamReader srCabecera = new StreamReader(filePathCabecera))
+                {
+                    string lineaCabecera;
+                    srCabecera.ReadLine(); 
+
+                    while ((lineaCabecera = srCabecera.ReadLine()) != null)
+                    {
+                        vectorCabecera = lineaCabecera.Split(';');
+                        nroComprobanteCabecera = vectorCabecera[0];
+                        razonSocial = vectorCabecera[1];
+                        fecha = vectorCabecera[2];
+
+                        lsbComprobantes.Items.Add($"Nro Comprobante: {nroComprobanteCabecera}");
+                        lsbComprobantes.Items.Add($"Razón Social: {razonSocial}");
+                        lsbComprobantes.Items.Add($"Fecha: {fecha}");
+
+                        //Muestro solo el encabezado del archivo CABECERA
+
+                        float montoTotal = 0;
+
+                        using (StreamReader srDetalle = new StreamReader(filePathDetalle))
+                        {
+                            string lineaDetalle;
+                            srDetalle.ReadLine(); 
+
+                            while ((lineaDetalle = srDetalle.ReadLine()) != null)
+                            {
+                                vectorDetalle = lineaDetalle.Split(';');
+                                nroComprobanteDetalle = vectorDetalle[0];
+
+                                if (nroComprobanteCabecera == nroComprobanteDetalle)
+                                {
+                                    descripcion = vectorDetalle[1];
+                                    var importe = float.Parse(vectorDetalle[2]);
+
+                                    lsbComprobantes.Items.Add($"  {descripcion}: {importe:C}");
+                                    montoTotal += importe;
+                                }
+                            }
+                        }
+
+                        lsbComprobantes.Items.Add($"TOTAL: {montoTotal:C}");
+                        lsbComprobantes.Items.Add(""); 
+                    }
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                lsbComprobantes.Items.Add("El archivo no se encontró: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                lsbComprobantes.Items.Add("Se produjo un error: " + ex.Message);
+            }
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
